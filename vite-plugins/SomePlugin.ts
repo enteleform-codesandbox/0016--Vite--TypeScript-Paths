c970d3748a57c8a1878319dc9f904ed677872da6
Enteleform <enteleform@gmail.com>
2022-09-10 06:21:09 -0400

    added example path + import

---
+++ vite-plugins/SomePlugin.ts
@@ -0,0 +1,5 @@
+import {Plugin} from "vite"
+
+export default function SomePlugin(): Plugin{
+	return {name: "some-plugin"}
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import SomePlugin from "VitePlugins/SomePlugin"
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+		SomePlugin(),
+		react(),
+	]
 })
